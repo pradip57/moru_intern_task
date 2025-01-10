87941ab31e67f19a43499324ccaa19d301b8cff8
Pradip <dellpidarp57@gmail.com>
2025-01-10 16:26:06 +0545

    done

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 const http = require("http");
-const app = require('./src/config/express.config')
+const app = require("./src/config/express.config");
 
 const server = http.createServer(app);
 
 const { config } = require("dotenv");
 config();
 
-server.listen(process.env.API_PORT, "127.0.0.1", (err) => {
-  console.log(`Server is running at port ${process.env.API_PORT}`);
+server.listen(process.env.PORT, "0.0.0.0", (err) => {
+  console.log(`Server is running at port ${process.env.PORT}`);
   console.log("Press CTRL + C to terminate the process");
 });
